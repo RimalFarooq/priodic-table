74abdd84e79bdf9dc66ef21e0f7ba0340a3b8afd
Rimal <155222148+RimalFarooq@users.noreply.github.com>
2026-04-02 19:41:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let con=document.querySelectorAll(".cc");
+// let body=document.querySelector("body");
+con.addEventListener("click",()=>{
+    console.log("clicked");
+});
+// con.onclick = ()=>{
+//     console.log("clicked");
+// }
